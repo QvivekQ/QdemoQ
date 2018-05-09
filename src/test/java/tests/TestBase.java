02491a23aff0ee330dade8a422e8e69b0ce5db30
QvivekQ <39054636+QvivekQ@users.noreply.github.com>
2018-05-09 12:46:10 +0530

    Update TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,7 +18,7 @@ public class TestBase {
 
 		BasicConfigurator.configure();
 
-		System.setProperty("webdriver.chrome.driver", "/home/qolsys/Driver/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
 		driver = new ChromeDriver();
 		// To maximize browser
 		driver.manage().window().maximize();
